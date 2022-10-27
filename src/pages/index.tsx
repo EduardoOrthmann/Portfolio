6d025d29ce60ea79df37ffe815b71b77bc305346
Eduardo <orthmann2811@gmail.com>
2022-10-27 19:04:54 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfólio</title>
+        <meta name="description" content="Personal portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+}
