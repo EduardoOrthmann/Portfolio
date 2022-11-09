a4e07a0d83f405df7d854de67cb2a577ca20ed7c
Eduardo <orthmann2811@gmail.com>
2022-11-09 16:03:31 -0300

    added animation to profile photo

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { PaperPlaneRight } from 'phosphor-react';
+import { motion } from 'framer-motion';
 import Button from '../components/Button';
 import { useGetProfileQuery } from '../graphql/generated';
 import styles from '../styles/HomePage.module.scss';
@@ -13,24 +14,33 @@ function HomePage() {
 
   return (
     <section id="home" className={styles.home}>
-      <div className={styles.titles}>
-        <h1>Oi, eu sou {data.profile.name}</h1>
+      <motion.div className={styles.titles} whileInView={{ opacity: [0, 1] }}>
+        <motion.h1
+          whileInView={{ x: [-100, 0] }}
+          transition={{ duration: 0.5 }}
+        >
+          Oi, eu sou {data.profile.name}
+        </motion.h1>
         <h5>{data.profile.headline}</h5>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias et
           sapiente odio nam nesciunt?
         </p>
         <Button icon={<PaperPlaneRight size={24} />}>Me Contate</Button>
-      </div>
+      </motion.div>
       <div>
-        <div className={styles.imageBg}>
+        <motion.div
+          className={styles.imageBg}
+          animate={{ y: [null, -20, 0] }}
+          transition={{ type: 'spring', repeat: Infinity, mass: 0.1, duration: 4 }}
+        >
           <Image
             src={data.profile.imageUrl.url}
             width={400}
             height={400}
             alt="foto de perfil"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
