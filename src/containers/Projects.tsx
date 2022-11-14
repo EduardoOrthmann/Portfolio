7dc331ee2b7b817e6ba16112ce5b331f6afb27ae
Eduardo <orthmann2811@gmail.com>
2022-11-14 17:43:53 -0300

    added tags to projects card

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -13,7 +13,9 @@ function Projects() {
 
   return (
     <section id="projects" className={styles.container}>
-      <h1 className={styles.headText}>Meus <span>Projetos</span></h1>
+      <h1 className={styles.headText}>
+        Meus <span>Projetos</span>
+      </h1>
       <ul className={styles.searchList}>
         {['UI/UX', 'ReactJS', 'Typescript', 'All'].map((name, i) => (
           <li key={i}>
@@ -25,15 +27,18 @@ function Projects() {
         {data.projects.map((project) => (
           <Card key={project.id}>
             <Card.Image>
-              {project.imageUrl.map((img, i) => (
-                <Image
-                  key={`${project.id}_${i}`}
-                  src={img.url}
-                  width={550}
-                  height={300}
-                  alt="foto do projeto"
-                />
-              ))}
+              <div className={styles.image}>
+                {project.imageUrl.map((img, i) => (
+                  <Image
+                    key={`${project.id}_${i}`}
+                    src={img.url}
+                    width={550}
+                    height={300}
+                    alt="foto do projeto"
+                  />
+                ))}
+                <span>{project.tags[0]}</span>
+              </div>
             </Card.Image>
             <Card.Content>
               <div className={styles.content}>
@@ -43,102 +48,6 @@ function Projects() {
             </Card.Content>
           </Card>
         ))}
-        {/* <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card>
-        <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card>
-        <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card>
-        <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card>
-        <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card>
-        <Card>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas
-            nemo quasi numquam, expedita ad reiciendis, iure, dolor explicabo
-            esse repudiandae laboriosam rem soluta nam nesciunt dicta facere
-            nihil architecto? Placeat aspernatur consequatur numquam ex, aut
-            excepturi provident rem sequi dicta quia porro inventore autem sunt
-            cumque laudantium debitis mollitia quidem, quisquam officiis,
-            architecto officia eveniet. Rem enim atque, sequi quis quam eveniet
-            ratione tenetur corrupti odit? Vel fuga iste minus facilis. Vel
-            laudantium debitis doloremque nostrum omnis ab ut, rerum ipsum
-            eaque! Ipsam consequuntur dicta delectus voluptatibus, porro ratione
-            veniam praesentium modi dignissimos, tenetur id veritatis recusandae
-            voluptatem blanditiis!
-          </span>
-        </Card> */}
       </div>
     </section>
   );
