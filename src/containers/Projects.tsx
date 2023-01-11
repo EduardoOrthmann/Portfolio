743c781153450fd2370397f692c6ce9b577bce93
Eduardo <orthmann2811@gmail.com>
2023-01-11 17:57:22 -0300

    completed projects section

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import { Code, Eye } from 'phosphor-react';
 import { useState } from 'react';
@@ -22,6 +22,14 @@ function Projects() {
     });
   });
 
+  const filteredProjects = data.projects.filter((project) => {
+    if (selectedFilter !== 'Todos') {
+      return project.tags.includes(selectedFilter);
+    }
+
+    return project;
+  });
+
   return (
     <section id="projects" className={styles.container}>
       <h1 className={styles.headText}>
@@ -41,62 +49,77 @@ function Projects() {
         ))}
       </ul>
       <div className={styles.projects}>
-        {data.projects.map((project) => (
-          <Card key={project.id}>
-            <Card.Image>
-              <div className={styles.image}>
-                <motion.div
-                  className={styles.imageOverlay}
-                  whileHover={{ opacity: [0, 1] }}
-                  transition={{
-                    duration: 0.25,
-                    ease: 'easeInOut',
-                    staggerChildren: 0.5,
-                  }}
-                >
-                  <a href={project.codeURL} target="_blank" rel="noreferrer">
+        <AnimatePresence>
+          {filteredProjects.map((project) => (
+            <motion.div
+              key={project.id}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Card>
+                <Card.Image>
+                  <div className={styles.image}>
                     <motion.div
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25 }}
+                      className={styles.imageOverlay}
+                      whileHover={{ opacity: [0, 1] }}
+                      transition={{
+                        duration: 0.25,
+                        ease: 'easeInOut',
+                        staggerChildren: 0.5,
+                      }}
                     >
-                      <Code size={32} />
-                    </motion.div>
-                  </a>
-                  {project.projectURL && (
-                    <a
-                      href={project.projectURL}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <motion.div
-                        whileHover={{ scale: [1, 0.9] }}
-                        transition={{ duration: 0.25 }}
+                      <a
+                        href={project.codeURL}
+                        target="_blank"
+                        rel="noreferrer"
                       >
-                        <Eye size={32} />
-                      </motion.div>
-                    </a>
-                  )}
-                </motion.div>
-                {project.imageUrl.map((img, i) => (
-                  <Image
-                    key={`${project.id}_${i}`}
-                    src={img.url}
-                    width={550}
-                    height={300}
-                    alt="foto do projeto"
-                  />
-                ))}
-                <span>{project.tags[0]}</span>
-              </div>
-            </Card.Image>
-            <Card.Content>
-              <div className={styles.content}>
-                <h1>{project.title}</h1>
-                <p>{project.description}</p>
-              </div>
-            </Card.Content>
-          </Card>
-        ))}
+                        <motion.div
+                          whileHover={{ scale: [1, 0.9] }}
+                          transition={{ duration: 0.25 }}
+                        >
+                          <Code size={32} />
+                        </motion.div>
+                      </a>
+                      {project.projectURL && (
+                        <a
+                          href={project.projectURL}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <motion.div
+                            whileHover={{ scale: [1, 0.9] }}
+                            transition={{ duration: 0.25 }}
+                          >
+                            <Eye size={32} />
+                          </motion.div>
+                        </a>
+                      )}
+                    </motion.div>
+                    {project.imageUrl.map((img, i) => (
+                      <Image
+                        key={`${project.id}_${i}`}
+                        src={img.url}
+                        width={550}
+                        height={300}
+                        alt="foto do projeto"
+                      />
+                    ))}
+                    <span>{project.tags[0]}</span>
+                  </div>
+                </Card.Image>
+                <Card.Content>
+                  <div className={styles.content}>
+                    <h1>{project.title}</h1>
+                    <p>{project.description}</p>
+                  </div>
+                </Card.Content>
+              </Card>
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
     </section>
   );
