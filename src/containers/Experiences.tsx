319e293e94a814b67e434849e0dd6b7d202709f1
Eduardo <orthmann2811@gmail.com>
2023-01-19 16:01:32 -0300

    completed experiences section

--- src/containers/Experiences.tsx
+++ src/containers/Experiences.tsx
@@ -1,4 +1,4 @@
-import { Briefcase, CalendarBlank, GraduationCap } from 'phosphor-react';
+import { Briefcase, CalendarBlank, CaretDown, GraduationCap } from 'phosphor-react';
 import { useState } from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import Button from '../components/Button';
@@ -49,24 +49,34 @@ function Experiences() {
                   key={experience.id}
                   icon={<Briefcase size={32} />}
                   iconStyle={{ background: '#313bac', color: '#fff' }}
-                  contentArrowStyle={{ borderRight: '7px solid  #edf2f8' }}
-                  contentStyle={{ background: '#edf2f8', color: '#000', borderTop: '5px solid #313bac' }}
+                  contentArrowStyle={{ borderRight: '7px solid #fff' }}
+                  contentStyle={{
+                    background: '#fff',
+                    color: '#000',
+                    borderTop: '5px solid #313bac',
+                    boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+                    display: 'grid',
+                    gap: '0.5rem',
+                  }}
                 >
                   <h3>{experience.role}</h3>
                   <details>
-                    <summary>{experience.company}</summary>
-                    <p>{experience.description}</p>
+                    <summary>
+                      <span className={styles.summaryTitle}>{experience.company}</span>
+                      <CaretDown size={20} />
+                    </summary>
+                    <div className={styles.summaryContent}>{experience.description}</div>
                   </details>
-                  <span className={styles.experienceDuration}>
+                  <div className={styles.experienceDuration}>
                     <CalendarBlank size={20} />
-                    <p>
+                    <span>
                       {experience.endDate
                         ? `${new Date(experience.startDate).getFullYear()} - ${new Date(
                             experience.endDate
                           ).getFullYear()}`
                         : `${new Date(experience.startDate).getFullYear()} - presente`}
-                    </p>
-                  </span>
+                    </span>
+                  </div>
                 </VerticalTimelineElement>
               ))
             : academicData.academicExperiences.map((experience) => (
@@ -74,23 +84,28 @@ function Experiences() {
                   key={experience.id}
                   icon={<Briefcase size={32} />}
                   iconStyle={{ background: '#313bac', color: '#fff' }}
-                  contentArrowStyle={{ borderRight: '7px solid  #edf2f8' }}
-                  contentStyle={{ background: '#edf2f8', color: '#000', borderTop: '5px solid #313bac' }}
+                  contentArrowStyle={{ borderRight: '7px solid #fff' }}
+                  contentStyle={{
+                    background: '#fff',
+                    color: '#000',
+                    borderTop: '5px solid #313bac',
+                    boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+                    display: 'grid',
+                    gap: '0.5rem',
+                  }}
                 >
                   <h3>{experience.title}</h3>
-                  <details>
-                    <summary>{experience.institute}</summary>
-                  </details>
-                  <span className={styles.experienceDuration}>
+                  <span className={styles.summaryTitle}>{experience.institute}</span>
+                  <div className={styles.experienceDuration}>
                     <CalendarBlank size={20} />
-                    <p>
+                    <span>
                       {experience.endDate
                         ? `${new Date(experience.startDate).getFullYear()} - ${new Date(
                             experience.endDate
                           ).getFullYear()}`
                         : `${new Date(experience.startDate).getFullYear()} - presente`}
-                    </p>
-                  </span>
+                    </span>
+                  </div>
                 </VerticalTimelineElement>
               ))}
         </VerticalTimeline>
