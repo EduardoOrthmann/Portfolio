8082720bdd203302355455dcaf88eb85781db486
Eduardo <orthmann2811@gmail.com>
2023-01-25 21:27:44 -0300

    added loading when send email is fetching

--- src/containers/Contact.tsx
+++ src/containers/Contact.tsx
@@ -1,4 +1,4 @@
-import { EnvelopeSimple, WhatsappLogo } from 'phosphor-react';
+import { CheckCircle, EnvelopeSimple, WhatsappLogo } from 'phosphor-react';
 import { useState } from 'react';
 import Button from '../components/Button';
 import { useGetContactQuery } from '../graphql/generated';
@@ -8,72 +8,94 @@ import styles from '../styles/Contact.module.scss';
 function Contact() {
   const { data } = useGetContactQuery();
   const [emailData, setEmailData] = useState({});
-  const { isLoading, error } = useFetch('/api/contact', emailData);
-
-  if (!data?.contact) {
-    return <p>...</p>;
-  }
+  const { isLoading, error, setIsFormSubmited } = useFetch('/api/contact', emailData);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsFormSubmited(true);
 
     const formData = new FormData(e.currentTarget);
     setEmailData(Object.fromEntries(formData));
   };
 
+  if (!data?.contact) {
+    return <p>...</p>;
+  }
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className={styles.headText}>Contato</h1>
       <div className={styles.content}>
-        <div>
-          <div className={styles.links}>
-            <div>
-              <EnvelopeSimple size={40} />
-              <strong>Email</strong>
-              <a
-                href={`mailto:${data.contact.email}?subject=Oportunidade%20para%20[cargo]%20na%20empresa%20[nome%20da%20empresa]`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {data.contact.email}
-              </a>
-            </div>
-            <div>
-              <WhatsappLogo size={40} />
-              <strong>Whatsapp</strong>
-              <a
-                href={`https://wa.me/${data.contact.phoneNumber.replace(/[^0-9]/g, '')}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {data.contact.phoneNumber}
-              </a>
-            </div>
-          </div>
-        </div>
         {isLoading ? (
           <div>
-            {isLoading && <div>Loading...</div>}
+            {isLoading && (
+              <svg
+                className={styles.spinner}
+                width="65px"
+                height="65px"
+                viewBox="0 0 66 66"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className={styles.path}
+                  fill="none"
+                  stroke-width="6"
+                  stroke-linecap="round"
+                  cx="33"
+                  cy="33"
+                  r="30"
+                ></circle>
+              </svg>
+            )}
             {error && <div>{error.message}</div>}
           </div>
         ) : (
-          <form className={styles.form} onSubmit={handleSubmit}>
+          <>
             <div>
-              <input type="text" name="subject" id="subject" required autoComplete="off" />
-              <label htmlFor="subject" title="Assunto" data-title="Assunto"></label>
+              <div className={styles.links}>
+                <div>
+                  <EnvelopeSimple size={40} />
+                  <strong>Email</strong>
+                  <a
+                    href={`mailto:${data.contact.email}?subject=Oportunidade%20para%20[cargo]%20na%20empresa%20[nome%20da%20empresa]`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {data.contact.email}
+                  </a>
+                </div>
+                <div>
+                  <WhatsappLogo size={40} />
+                  <strong>Whatsapp</strong>
+                  <a
+                    href={`https://wa.me/${data.contact.phoneNumber.replace(/[^0-9]/g, '')}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {data.contact.phoneNumber}
+                  </a>
+                </div>
+              </div>
             </div>
-            <div>
-              <input type="email" name="from" id="from" required autoComplete="off" />
-              <label htmlFor="from" title="Seu Email" data-title="Seu Email"></label>
-            </div>
-            <div>
-              <textarea name="text" id="text" required autoComplete="off"></textarea>
-              <label htmlFor="text" title="Sua Mensagem" data-title="Sua Mensagem"></label>
-            </div>
-            <Button color="purple" type="submit">
-              Enviar Mensagem
-            </Button>
-          </form>
+
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <div>
+                <input type="text" name="subject" id="subject" required autoComplete="off" />
+                <label htmlFor="subject" title="Assunto" data-title="Assunto"></label>
+              </div>
+              <div>
+                <input type="email" name="from" id="from" required autoComplete="off" />
+                <label htmlFor="from" title="Seu Email" data-title="Seu Email"></label>
+              </div>
+              <div>
+                <textarea name="text" id="text" required autoComplete="off"></textarea>
+                <label htmlFor="text" title="Sua Mensagem" data-title="Sua Mensagem"></label>
+              </div>
+              <Button color="purple" type="submit">
+                Enviar Mensagem
+              </Button>
+            </form>
+          </>
         )}
       </div>
     </section>
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -3,33 +3,33 @@ import { useState, useEffect } from 'react';
 function useFetch(url: string, body: object) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const [isFormSubmited, setIsFormSubmited] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
+    if (isFormSubmited) {
+      const fetchData = async () => {
+        setIsLoading(true);
 
-      try {
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body),
-        });
+        try {
+          await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+          });
 
-        const data = await response.json();
-        console.log(data);
-      } catch (err) {
-        setError(err as Error);
-        console.log(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    if (Object.entries(body).length !== 0) {
+          setIsFormSubmited(false);
+        } catch (err) {
+          setError(err as Error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
       fetchData();
     }
-  }, [body, url]);
+  }, [body, url, isFormSubmited]);
 
-  return { isLoading, error };
+  return { isLoading, error, setIsFormSubmited };
 }
 
 export default useFetch;
